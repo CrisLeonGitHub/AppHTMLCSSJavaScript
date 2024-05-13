ffbb9827aa92fa950eadb1c5a102b3eec467be41
CrisLeonGitHub <cleonfuentes@gmail.com>
2024-05-13 14:39:53 -0400

    ingreso comentarios

--- JavaScript/curso-de-javascript-main/capitulo 6/problema 1/codigo.js
+++ JavaScript/curso-de-javascript-main/capitulo 6/problema 1/codigo.js
@@ -21,6 +21,7 @@ for (var i = 1; i <= 20; i++) {
     });
     div.tabIndex = i;
     div.classList.add(`item-${i}`,'flex-item');
+	/* esto ordena y deja al inicio lo que se quiere*/
     div.innerHTML = ...llave;
 	documentFragment.appendChild(div);
 }
